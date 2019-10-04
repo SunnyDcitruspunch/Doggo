3a9332a6a88cc2c9ff5310d22120ea85e0e29e28
SunnyDcitruspunch <wiwisun18@gmail.com>
2019-10-04 17:16:05 -0600

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Index from "../src/components/Index";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Index />
+    </div>
+  );
+};
+
+export default App;
