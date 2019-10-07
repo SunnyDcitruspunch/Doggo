5003e6a928a52c6ff275028a9623e3050bbc8fe2
SunnyDcitruspunch <wiwisun18@gmail.com>
2019-10-07 16:09:37 -0600

    working on navbar

--- src/components/Home/Greetings.tsx
+++ src/components/Home/Greetings.tsx
@@ -1,43 +1,25 @@
 import React from "react";
 import "../../fonts/LovelexieHandwritten.ttf";
 import "./Greetings.scss";
-import Doggo from "../../images/adog.jpg";
 
 const Greetings = () => {
   return (
     <React.Fragment>
-      <div id="doggoimg">
-        <img src={Doggo} alt="doggo" />
-      </div>
-      <div
-        id="desc"
-        style={{ fontFamily: "LoveLexi" }}
-      >
+      <div id="doggoimg" />
+      <div id="desc" style={{ fontFamily: "LoveLexi" }}>
         <div className="card">
-          <h4>
-            <b>Who we are</b>
-          </h4>
-          <p>Architect & Engineer</p>
+          <h3>
+            <b>Adopt a Dog</b>
+          </h3>
         </div>
         <div className="card">
-          <h4>
-            <b>Where we are</b>
-          </h4>
-          <p>Architect & Engineer</p>
+          <h3>Adopt a Cat</h3>
         </div>
         <div className="card">
-          <h4>
-            <b>Where we are</b>
-          </h4>
-          <p>Architect & Engineer</p>
-        </div>
-        <div className="card">
-          <h4>
-            <b>Where we are</b>
-          </h4>
-          <p>Architect & Engineer</p>
+          <h3>I Love Both :)</h3>
         </div>
       </div>
+      <div id="doggoimg" />
     </React.Fragment>
   );
 };
--- src/components/Home/Nav.tsx
+++ src/components/Home/Nav.tsx
@@ -9,7 +9,7 @@ const Nav = () => {
   return (
     <React.Fragment>
       <nav
-        className="navbar navbar-expand-lg"
+        id="nav"
         style={{ display: "block", textAlign: "center", verticalAlign: "top" }}
       >
         <div
@@ -23,49 +23,25 @@ const Nav = () => {
               textDecoration: "none",
               textAlign: "center",
               fontSize: "40px",
-              color: "#F7882f"
+              color: "#F7882f",
+              paddingTop: "1vw"
             }}
           >
             Guava's Animal Playground
           </h3>
         </div>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarColor03"
-          aria-controls="navbarColor03"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        {/* <div className="collapse navbar-collapse" id="navbarColor03">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-link">
-              <Link to="/adoption">
-                <span style={{ fontFamily: "Pecita" }}>Adoption</span>
-              </Link>
-            </li>
-            <li className="nav-link">
-              <Link to="/donation">
-                {" "}
-                <span style={{ fontFamily: "Pecita" }}>Donation </span>
-              </Link>
-            </li>
-            <li className="nav-link">
-              <Link to="/create">
-                <span style={{ fontFamily: "Pecita" }}>Create New Post </span>
-              </Link>
-            </li>
-            <li className="nav-link">
-              <Link to="/about">
-                <span style={{ fontFamily: "Pecita" }}>About </span>
-              </Link>
-            </li>
-          </ul>
-        </div> */}
+        <div id="menu">
+          <Link to="/adoption" style={{ textDecoration: 'none', color:"#f7882f" }}>
+            <h4>Find my new best friend</h4>
+          </Link>
+          <Link to="/donation" style={{ textDecoration: 'none', color:"#f7882f" }}>
+            <h4>Donation</h4>
+          </Link>
+          <Link to="/about" style={{ textDecoration: 'none', color:"#f7882f" }}>
+            <h4>About Us</h4>
+          </Link>
+          <h4>Contact</h4>
+        </div>
       </nav>
     </React.Fragment>
   );
